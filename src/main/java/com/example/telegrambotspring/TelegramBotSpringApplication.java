bc0586915bbdc991deab6d60c1cae4b6e1b53d0f
Stas Pasynkov <s.pasynkov@gmail.com>
2020-03-21 11:17:59 +0200

    Initial commit

---
+++ src/main/java/com/example/telegrambotspring/TelegramBotSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.telegrambotspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotSpringApplication.class, args);
+	}
+
+}
