6e74dc50d0eed8f0e3b733b97423e6acb347eeda
alex_man <alexmnsbs@gmail.com>
2020-04-01 15:55:47 +0300

    первая часть proceedTelegramApiWebhook

--- src/main/java/com/example/telegrambotspring/controllers/TelegramApiListenerController.java
+++ src/main/java/com/example/telegrambotspring/controllers/TelegramApiListenerController.java
@@ -6,9 +6,18 @@ import org.springframework.web.bind.annotation.*;
 
 import com.example.telegrambotspring.services.TelegramWebhooksService;
 
+/**
+ * The class ...
+ * <b>webhooksService</b>
+ * @author  Stas Pasynkov
+ * @see     com.example.telegrambotspring.controllers.TelegramApiListenerController
+ * @version 1.0
+ */
+
 @RestController
 @RequestMapping(value = "/rest", method = RequestMethod.POST, produces = "application/json")
 public class TelegramApiListenerController {
+	/** Поле  */
 	private TelegramWebhooksService webhooksService;
 
 	@Autowired
@@ -16,6 +25,13 @@ public class TelegramApiListenerController {
 		this.webhooksService = webhooksService;
 	}
 
+
+/**
+ * Метод обработки запросов от бота в режиме webhooks - бот реагирует только на входящие сообщения
+ * @param botToken - уникальный идентификатор, ключ для доступа к боту
+ * @param jsonString - текст сообщения в формате json
+ * @return возвращает
+ */
 	@RequestMapping("/{botToken}")
 	public String proceedTelegramApiWebhook(@PathVariable String botToken, @RequestBody String jsonString) {
 		return webhooksService.proceedTelegramApiWebhook(botToken, jsonString);
