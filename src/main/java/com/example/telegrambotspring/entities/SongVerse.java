87e78122f9145b3db39e2714112dae47a1dbf9d1
alex_man <alexmnsbs@gmail.com>
2020-04-05 10:42:34 +0300

    javadoc на class SongVerse готов

--- src/main/java/com/example/telegrambotspring/entities/SongVerse.java
+++ src/main/java/com/example/telegrambotspring/entities/SongVerse.java
@@ -9,6 +9,7 @@ import java.util.Objects;
 
 /**
  * The class описывает структуру формата хранения данных песни
+ * в этой структуре хранится только один куплет песни
  * <b>id</b> and <b>artist</b> and <b>song</b> and <b>verseId</b> and <b>text</b>
  * @author  Stas Pasynkov
  *
@@ -27,12 +28,24 @@ public class SongVerse implements Comparable<SongVerse> {
 	private String song;
 	/** номер версии песни */
 	private int verseId;
-	/** текс песни */
+	/** текс песни - тут только одни куплет*/
 	private List<String> text;
 
+
+	/**
+	 * Конструктор по умолчанию - создание нового объекта
+	 */
 	public SongVerse() {
 	}
 
+
+	/**
+	 * Конструктор - создание нового объекта с определенными значениями
+	 * @param artist - имя исполнителя
+	 * @param song - название песни
+	 * @param verseId - номер версии песни
+	 * @param text - текс песни - куплет - передается в виде списка строк
+	 */
 	public SongVerse(String artist, String song, int verseId, List<String> text) {
 		this.artist = artist;
 		this.song = song;
@@ -40,50 +53,107 @@ public class SongVerse implements Comparable<SongVerse> {
 		this.text = text;
 	}
 
+
+	/**
+	 * Конструктор - создание нового объекта с определенными значениями
+	 * @param artist - имя исполнителя
+	 * @param song - название песни
+	 * @param verseId - номер версии песни
+	 * @param text - текс песни - куплет - передается в виде массива строк
+	 */
 	public SongVerse(String artist, String song, int verseId, String... text) {
 		this(artist, song, verseId, Arrays.asList(text));
 	}
 
+
+	/**
+	 * метод - чтение идентификатора песни
+	 * @return возвращает строку - идентификатор песни
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * метод - запись идентификатора песни
+	 * @param id - идентификатор песни
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * метод - чтение имени исполнителя
+	 * @return возвращает строку - имя исполнителя
+	 */
 	public String getArtist() {
 		return artist;
 	}
 
+	/**
+	 * метод - запись имени исполнителя
+	 * @param artist - имя исполнителя
+	 */
 	public void setArtist(String artist) {
 		this.artist = artist;
 	}
 
+	/**
+	 * метод - чтение названия песни
+	 * @return возвращает строку - название песни
+	 */
 	public String getSong() {
 		return song;
 	}
 
+	/**
+	 * метод - запись названия песни
+	 * @param song - название песни
+	 */
 	public void setSong(String song) {
 		this.song = song;
 	}
 
+
+	/**
+	 * метод - чтение номера версии песни
+	 * @return возвращает целое число - номер версии песни
+	 */
 	public int getVerseId() {
 		return verseId;
 	}
 
+	/**
+	 * метод - запись номера версии песни
+	 * @param verseId - номер версии песни
+	 */
 	public void setVerseId(int verseId) {
 		this.verseId = verseId;
 	}
 
+
+	/**
+	 * метод - чтение текста песни - одного куплета
+	 * @return возвращает список строк - текс песни - один куплет
+	 */
 	public List<String> getText() {
 		return text;
 	}
 
+	/**
+	 * метод - запись текста песни - одного куплета
+	 * @param text - текс песни
+	 */
 	public void setText(List<String> text) {
 		this.text = text;
 	}
 
+
+	/**
+	 * переопределяем метод equals
+	 * @return возвращает boolean результат сравнения песен по следующим полям
+	 * verseId, artist, song, text
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
@@ -95,6 +165,11 @@ public class SongVerse implements Comparable<SongVerse> {
 				Objects.equals(text, that.text);
 	}
 
+
+	/**
+	 * переопределяем метод hashCode
+	 * @return возвращает hashCode Objects
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(artist, song, verseId, text);
@@ -106,6 +181,10 @@ public class SongVerse implements Comparable<SongVerse> {
 				id, artist, song, verseId, text);
 	}
 
+	/**
+	 * переопределяем метод compareTo
+	 * @return возвращает код сравнения: 0 объекты равны <>0 - не равны
+	 */
 	@Override
 	public int compareTo(SongVerse that) {
 		int artistComparision = this.artist.compareTo(that.artist);
