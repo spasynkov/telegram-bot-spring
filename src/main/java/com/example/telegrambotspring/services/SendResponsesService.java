375c24252470607092ff6e063fe560b1d6a8e31e
Mikhail Alekseev <mikhailalekseev1990@gmail.com>
2020-04-11 18:38:45 +0300

    #31Change-logic-of-searching-and-sending-reply
    Need fix error services.TelegramBotApiRequestsSender.sendGet(TelegramBotApiRequestsSender.java:89) [classes/:na]

--- src/main/java/com/example/telegrambotspring/services/SendResponsesService.java
+++ src/main/java/com/example/telegrambotspring/services/SendResponsesService.java
@@ -1,28 +1,28 @@
 package com.example.telegrambotspring.services;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
+import com.example.telegrambotspring.entities.Chat;
+import com.example.telegrambotspring.entities.Message;
+import com.example.telegrambotspring.entities.Received;
+import com.example.telegrambotspring.entities.bots.SongsBot;
+import com.example.telegrambotspring.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
-import com.example.telegrambotspring.entities.Chat;
-import com.example.telegrambotspring.entities.bots.SongsBot;
-import com.example.telegrambotspring.utils.Pair;
-import com.example.telegrambotspring.utils.Utils;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SendResponsesService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(SendResponsesService.class);
 
 	private TelegramBotApiRequestsSender requestsSender;
-	private Map<Chat, Pair<Long, String>> answersForChats;
+	private Received received;
 
 	@Value("${app.send-message-latency.group}")
 	private long sendMessageLatencyGroup;
@@ -31,12 +31,12 @@ public class SendResponsesService {
 	private long sendMessageLatencyDirect;
 
 	@Autowired
-	public SendResponsesService(@Qualifier("answersForChats") Map<Chat, Pair<Long, String>> answersForChats,
+	public SendResponsesService(Received received,
 	                            SongsBot bot,    // TODO: hardcoded bot instance. rewrite with chat response object
 	                            TelegramBotApiRequestsSender requestsSender, ThreadPoolTaskExecutor executor) {
-
-		this.answersForChats = answersForChats;
+		this.received = received;
 		this.requestsSender = requestsSender;
+
 
 		Runnable runnable = () -> {
 			try {
@@ -56,12 +56,13 @@ public class SendResponsesService {
 	}
 
 	private void sendResponses(SongsBot bot) {
-		Iterator<Map.Entry<Chat, Pair<Long, String>>> iterator = answersForChats.entrySet().iterator();
+		Iterator<Map.Entry<Chat, List<Message>>> iterator = received.getIncomingMessage().entrySet().iterator();
 		while (iterator.hasNext()) {
-			Map.Entry<Chat, Pair<Long, String>> entry = iterator.next();
+			Map.Entry<Chat, List<Message>> entry = iterator.next();
 			int chatId = entry.getKey().getChatId();
-			long lastMessageTime = entry.getValue().getFirst();
-			String preparedResponse = entry.getValue().getSecond();
+			String textMessage = entry.getValue().get(entry.getValue().size() - 1).getText();
+			long lastMessageTime = entry.getValue().get(entry.getValue().size() - 1).getData();
+
 
 			long latency = entry.getKey().isGroup()
 					? sendMessageLatencyGroup
@@ -69,7 +70,7 @@ public class SendResponsesService {
 
 			if (lastMessageTime * Utils.MILLIS_MULTIPLIER < System.currentTimeMillis() - latency) {
 				try {
-					requestsSender.sendMessage(bot, chatId, preparedResponse);
+					requestsSender.sendMessage(bot, chatId, textMessage);
 					iterator.remove();
 				} catch (Exception e) {
 					LOGGER.error("Unable to send response or delete chat from map", e);
