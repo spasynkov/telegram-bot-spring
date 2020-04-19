e83d2b237b77a5f51d8c43d15911f52704e8a1ca
Stas Pasynkov <s.pasynkov@gmail.com>
2020-04-19 19:25:15 +0300

    Added test that all needed beans are exists in application context

--- src/test/java/com/example/telegrambotspring/TelegramBotSpringApplicationTests.java
+++ src/test/java/com/example/telegrambotspring/TelegramBotSpringApplicationTests.java
@@ -1,13 +1,67 @@
 package com.example.telegrambotspring;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.telegrambotspring.controllers.MainController;
+import com.example.telegrambotspring.controllers.RestController;
+import com.example.telegrambotspring.controllers.TelegramApiListenerController;
+import com.example.telegrambotspring.entities.Chat;
+import com.example.telegrambotspring.entities.bots.SongsBot;
+import com.example.telegrambotspring.services.ResponseService;
+import com.example.telegrambotspring.services.SendResponsesService;
+import com.example.telegrambotspring.services.TelegramBotApiRequestsSender;
+import com.example.telegrambotspring.services.TelegramWebhooksService;
+import com.example.telegrambotspring.utils.Pair;
 
 @SpringBootTest
 class TelegramBotSpringApplicationTests {
 
+	@Autowired
+	private MainController mainController;
+
+	@Autowired
+	private RestController restController;
+
+	@Autowired
+	private TelegramApiListenerController apiListenerController;
+
+	@Autowired
+	private Map<Chat, Pair<Long, String>> answersForChats;
+
+	@Autowired
+	private SongsBot songsBot;
+
+	@Autowired
+	private ResponseService responseService;
+
+	@Autowired
+	private SendResponsesService sendResponsesService;
+
+	@Autowired
+	private TelegramBotApiRequestsSender telegramBotApiRequestsSender;
+
+	@Autowired
+	private TelegramWebhooksService telegramWebhooksService;
+
 	@Test
 	void contextLoads() {
+		assertThat(mainController).isNotNull();
+		assertThat(restController).isNotNull();
+		assertThat(apiListenerController).isNotNull();
+
+		assertThat(answersForChats).isNotNull();
+		assertThat(songsBot).isNotNull();
+
+		assertThat(responseService).isNotNull();
+		assertThat(sendResponsesService).isNotNull();
+		assertThat(telegramBotApiRequestsSender).isNotNull();
+		assertThat(telegramWebhooksService).isNotNull();
 	}
 
 }
