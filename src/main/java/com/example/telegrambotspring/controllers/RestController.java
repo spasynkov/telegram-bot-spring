69e75c8b0f27c548e5d109e041384c74449bd0ff
alex_man <alexmnsbs@gmail.com>
2020-04-19 16:11:53 +0300

    Merge branch 'dev' of C:\Projects\tlg\telegram-bot-spring with conflicts.

--- src/main/java/com/example/telegrambotspring/controllers/RestController.java
+++ src/main/java/com/example/telegrambotspring/controllers/RestController.java
@@ -5,41 +5,96 @@ import com.example.telegrambotspring.services.DatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+/**
+ * The class уровня (слоя) Controller
+ * обработка запросов web интерфейса
+ * <b>service</b>
+ * @author  Stas Pasynkov
+ *
+ * @see     com.example.telegrambotspring.controllers.MainController
+ * @see     com.example.telegrambotspring.controllers.TelegramApiListenerController
+ * @version 1.0.1
+ */
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = "/rest", produces = "application/json")
 public class RestController {
+    /** объект класса для работы с БД следующего уровня - сервисный  - сервисный объект для работы с БД*/
 	private DatabaseService service;
 
+	/**
+	 * Конструктор - создание нового объекта с определенными значениями
+	 * @param service сервисный объект для работы с БД
+	 */
 	@Autowired
 	public RestController(DatabaseService service) {
 		this.service = service;
 	}
 
+    /**
+     * Метод получения полного списка песен
+     * @return возвращает список всех песен с текстом и др. параметрами типа SongVerse одной строкой
+     */
 	@GetMapping("/all")
 	public String getAll() {
 		return service.getAll().toString();
 	}
 
+	/**
+	 * Метод получения полного списка песен данного исполнителя
+	 * @param artistName имя исполнителя
+	 * @return возвращает список всех песен данного исполнителя с текстом и др. параметрами типа SongVerse одной строкой
+	 */
 	@GetMapping("/all/artist/{artistName}")
 	public String getAllByArtist(@PathVariable String artistName) {
 		return service.getAllByArtist(artistName).toString();
 	}
 
+	/**
+	 * Метод получения полного списка всех версий песен данного исполнителя и названия песни
+	 * @param artistName имя исполнителя
+	 * @param songName название песни
+	 * @return возвращает список всех версий песен данного исполнителя и названия песни с текстом и др. параметрами типа SongVerse одной строкой
+	 */
 	@GetMapping("/all/artist/{artistName}/song/{songName}")
 	public String getAllByArtist(@PathVariable String artistName, @PathVariable String songName) {
 		return service.getAllByArtistAndSong(artistName, songName).toString();
 	}
 
+	/**
+	 * Метод посылает запрос на добавления новой песни в БД
+	 * @param verse объект типа SongVerse со всеми атрибутами
+	 * @param artistName имя исполнителя
+	 * @param songName название песни
+	 * @return возвращает объект типа SongVerse
+	 * текст песни и все ее атрибуды с присвоенным Id из БД одной строкой
+	 */
 	@PutMapping("/all/artist/{artistName}/song/{songName}")
 	public String addSong(@RequestBody SongVerse verse, @PathVariable String artistName, @PathVariable String songName) {
 		return service.addSong(verse, artistName, songName).toString();
 	}
 
+	/**
+	 * Метод посылает запрос на изменение песни в БД
+	 * @param verse объект типа SongVerse со всеми атрибутами
+	 * @param artistName имя исполнителя
+	 * @param songName название песни
+	 * @return возвращает объект типа SongVerse
+	 * текст песни и все ее атрибуды с присвоенным Id из БД одной строкой
+	 */
 	@PostMapping("/all/artist/{artistName}/song/{songName}")
 	public String editSong(@RequestBody SongVerse verse, @PathVariable String artistName, @PathVariable String songName) {
 		return service.editSong(verse, artistName, songName).toString();
 	}
 
+	/**
+	 * Метод посылает запрос на удаление песни из БД
+	 * @param verse объект типа SongVerse со всеми атрибутами
+	 * @param artistName имя исполнителя
+	 * @param songName название песни
+	 * @return возвращает пустой объект типа SongVerse в виде стоки
+	 */
 	@DeleteMapping("/all/artist/{artistName}/song/{songName}")
 	public String deleteSong(@RequestBody SongVerse verse, @PathVariable String artistName, @PathVariable String songName) {
 		return service.deleteSong(verse, artistName, songName).toString();
--- src/main/java/com/example/telegrambotspring/controllers/TelegramApiListenerController.java
+++ src/main/java/com/example/telegrambotspring/controllers/TelegramApiListenerController.java
@@ -6,9 +6,31 @@ import org.springframework.web.bind.annotation.*;
 
 import com.example.telegrambotspring.services.TelegramWebhooksService;
 
+/**
+ * The class уровня (слоя) REST Controller
+ * в данном классе будет реализована логика обработки клиентских запросов
+ * REST это передача состояний ресурса между сервером и клиентом
+ * В REST почти во всех случаях используется протокол HTTP
+ * ресурсы в REST могут быть представлены в любой форме — JSON, XML, текст, или даже HTML
+ * Ресурс в REST — это все, что может быть передано между клиентом и сервером
+ * Действия в REST определяются http-методами.
+ * Get, Post, Put, Delete, Patch, и другие.
+ * Самые часто-используемые обозначаются аббревиатурой CRUD:
+ * Create — POST
+ * Read — GET
+ * Update — PUT
+ * Delete — DELETE
+ * <b>webhooksService</b>
+ * @author  Stas Pasynkov
+ * @see     com.example.telegrambotspring.controllers.MainController
+ * @see     com.example.telegrambotspring.controllers.RestController
+ * @version 1.0.1
+ */
 @RestController
 @RequestMapping(value = "/rest", method = RequestMethod.POST, produces = "application/json")
 public class TelegramApiListenerController {
+
+	/** Поле ??? */
 	private TelegramWebhooksService webhooksService;
 
 	@Autowired
@@ -16,6 +38,14 @@ public class TelegramApiListenerController {
 		this.webhooksService = webhooksService;
 	}
 
+
+  /**
+   * Метод обработки запросов от бота в режиме webhooks - бот реагирует только на входящие сообщения
+   * @param botToken - уникальный идентификатор, ключ для доступа к боту
+   * @param jsonString - текст сообщения в формате json
+   * @return возвращает - результат обработки сообщения с чата
+   * @see com.example.telegrambotspring.BotRunner#run(String...)
+   */
 	@RequestMapping("/{botToken}")
 	public String proceedTelegramApiWebhook(@PathVariable String botToken, @RequestBody String jsonString) {
 		return webhooksService.proceedTelegramApiWebhook(botToken, jsonString);
