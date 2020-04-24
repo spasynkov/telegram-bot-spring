f81ca63f479623af453eda0308bb94591c4b8f87
alex_man <alexmnsbs@gmail.com>
2020-04-24 17:59:53 +0300

    #15 SongVerse

--- src/main/java/com/example/telegrambotspring/entities/SongVerse.java
+++ src/main/java/com/example/telegrambotspring/entities/SongVerse.java
@@ -1,11 +1,21 @@
 package com.example.telegrambotspring.entities;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.annotation.Id;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+@Slf4j
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(exclude = { "id" })
 public class SongVerse implements Comparable<SongVerse> {
 	@Id
 	private String id;
@@ -27,68 +37,6 @@ public class SongVerse implements Comparable<SongVerse> {
 
 	public SongVerse(String artist, String song, int verseId, String... text) {
 		this(artist, song, verseId, Arrays.asList(text));
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getArtist() {
-		return artist;
-	}
-
-	public void setArtist(String artist) {
-		this.artist = artist;
-	}
-
-	public String getSong() {
-		return song;
-	}
-
-	public void setSong(String song) {
-		this.song = song;
-	}
-
-	public int getVerseId() {
-		return verseId;
-	}
-
-	public void setVerseId(int verseId) {
-		this.verseId = verseId;
-	}
-
-	public List<String> getText() {
-		return text;
-	}
-
-	public void setText(List<String> text) {
-		this.text = text;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		SongVerse that = (SongVerse) o;
-		return verseId == that.verseId &&
-				Objects.equals(artist, that.artist) &&
-				Objects.equals(song, that.song) &&
-				Objects.equals(text, that.text);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(artist, song, verseId, text);
-	}
-
-	@Override
-	public String toString() {
-		return String.format("SongVerse{id='%s', artist='%s', song='%s', verseId=%d, text=%s}",
-				id, artist, song, verseId, text);
 	}
 
 	@Override
